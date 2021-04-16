3f36ce50a6714950a3d90776384ee281b863652e
Sudhanshu Bisht <sudhanshu@delimp.com>
2021-04-16 12:24:22 +0530

    remove unwanter files

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \Fruitcake\Cors\HandleCors::class,
-        \App\Http\Middleware\Cors::class,
         \App\Http\Middleware\TrustProxies::class,
 
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
