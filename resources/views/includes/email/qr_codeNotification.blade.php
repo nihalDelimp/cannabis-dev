4da6ee37e60b3146f18f852fc06da12ac368fdfc
akhlaq <akhlaq@delimp.com>
2022-10-11 14:58:25 +0530

    modification on emil template

--- resources/views/includes/email/qr_codeNotification.blade.php
+++ resources/views/includes/email/qr_codeNotification.blade.php
@@ -10,7 +10,7 @@
                 <path d="M5.867 6.75l5.28 4.928a1.25 1.25 0 001.706 0l5.28-4.928H5.867zM19.25 7.76l-5.374 5.015a2.75 2.75 0 01-3.752 0L4.75 7.759V17c0 .138.112.25.25.25h14a.25.25 0 00.25-.25V7.76zM3.25 7c0-.966.784-1.75 1.75-1.75h14c.966 0 1.75.784 1.75 1.75v10A1.75 1.75 0 0119 18.75H5A1.75 1.75 0 013.25 17V7z"></path>
             </svg>
             <div class="puco-text text_textBlock__WUY_8">
-                <p class="puco-text"><em>Hi {{$body['name']}},</em><br><br><em>Have you heard the news? Something big is coming from {{$body['event_name']}}.</em><br><br><em>On {{ $body['event_time']}}, we’ll be launching our [Insert new product or service name] live at [Location].</em><br><br><em>Be the first to learn everything about [Insert new product or service], as well as receive exclusive access to [Benefit of attending].</em><br><br><em>Get on the list today!</em>
+                <p class="puco-text"><em>Hi {{$body['name']}},</em><br><br><em>Have you heard the news? Something big is coming from {{$body['event_name']}}.</em><br><br><em>On {{ $body['event_time']}}, we’ll be live casting  our production live at cannabis capitol prodcution room.</em><br><br><em>Be the part of it to learn everything about the {{$body['event_name']}}.</em><br><br><em>Get on the list today!</em>
                     <br><br><em>
                         {{ $body['qr_code'] }} 
                     </em></p>
