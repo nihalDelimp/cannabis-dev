8ad2b0cbc3cbbaf3c58a9e902f15bbed4651098c
akhlaq <akhlaq@delimp.com>
2022-10-14 17:53:23 +0530

    create new route , registere user list, and api for validate production..

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Event;
+use App\Models\EventJoinList;
 use App\Models\User;
 use App\Models\Category;
 use App\Models\Tag;
